d29566e3fe22a47f6207806069f9c4c419e4c555
Lim Jing Zhi <limjingzhi.cs@gmail.com>
2020-12-10 17:49:17 +0800

    use axios for sign in api call

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,14 +1,26 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+import { setUserSession } from '../Utils/Common';
  
 function Login(props) {
+  const [loading, setLoading] = useState(false);
   const username = useFormInput('');
   const password = useFormInput('');
   const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
  
   // handle button click of login form
   const handleLogin = () => {
-    props.history.push('/dashboard');
+    setError(null);
+    setLoading(true);
+    axios.post('http://localhost:4000/users/signin', { username: username.value, password: password.value }).then(response => {
+      setLoading(false);
+      setUserSession(response.data.token, response.data.user);
+      props.history.push('/dashboard');
+    }).catch(error => {
+      setLoading(false);
+      if (error.response.status === 401) setError(error.response.data.message);
+      else setError("Something went wrong. Please try again later.");
+    });
   }
  
   return (
@@ -39,5 +51,5 @@ const useFormInput = initialValue => {
     onChange: handleChange
   }
 }
- 
+
 export default Login;
